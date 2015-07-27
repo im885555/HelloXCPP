8c48889b56183e357056ef98040fdf4f7d4535a8
Gene <im885555@gmail.com>
2015-07-27 10:34:53 +0800

    Initial Commit

---
+++ HelloXCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloXCPP
+//
+//  Created by Gene on 2015/7/27.
+//  Copyright (c) 2015年 Gene. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
